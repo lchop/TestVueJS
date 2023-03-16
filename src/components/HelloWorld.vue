5b68b7cd8a8d4e7323a4f64897855419cb1a1065
you@example.com <you@example.com>
2023-03-16 11:34:30 +0100

    11:34 No error in the check code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,13 @@
 <template>
   <div class="hello">
-    <!-- <h1>{{ title }}</h1> -->
-    <!-- <form  action="#" method="post">Login</form>
-    <textarea name="Test" id="test" cols="50" rows="2.5"></textarea>
-    <button onsubmit="console.log(test);" style="position: fixed;width: 60px; height: 25px;left: 663px;top: 353px;">Login</button>
-   -->
-    <!-- <form method="get" onsubmit="Verifie(this)">
-      <label for="pseudo">Pseudo : </label> <input type="text" id="pseudo" name="pseudo" /><br />
-      <label for="pass">Mot de passe : </label> <input type="password" id="pass" name="pass" /><br />
-      <input type="submit" value="Connexion" />
-    </form> -->
-
-
     <form>
-			<input type="text" id="username" placeholder="Choose Username">
-			<input type="password" id="password" placeholer="Choose Password">
-			<button type="button" onclick="getInfo()">Click Me</button>
-		</form>
-    
-  
-    <!-- <button @click="handleClick">Click me!</button> -->
-
-  
-  
+      <input type="text" id="username" placeholder="Choose Username">
+      <input type="password" id="password" placeholer="Choose Password">
+      <button type="button" @click="getInfo">Click Me</button>
+    </form>  
   </div>
 </template>
-
+  
 <script>
 export default {
   methods: {
@@ -50,26 +32,38 @@ export default {
   }
 }
 
-
 var objPeople = [
-	{ // Object @ 0 index
-		username: "sam",
-		password: "codify"
-	},
-	{ // Object @ 1 index
-		username: "matt",
-		password: "academy"
-	},
-	{ // Object @ 2 index
-		username: "chris",
-		password: "forever"
-	}
+    { // Object @ 0 index
+        username: "test",
+        password: "test"
+    },
+    { // Object @ 1 index
+        username: "Admin",
+        password: "admin"
+    }
 
- ]
-
-
-
+]
 </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
