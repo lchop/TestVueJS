813eeca5c89efa830d1373ec4f29e1b92f1e83b2
you@example.com <you@example.com>
2023-03-16 15:05:56 +0100

    config vue-router

--- src/main.js
+++ src/main.js
@@ -2,3 +2,31 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import * as VueRouter from 'vue-router'
+import Home from './pages/Home.vue'
+import About from './pages/About.vue'
+
+const router = VueRouter.createRouter({
+history: VueRouter.createWebHistory(),
+routes: [
+    {
+    path: '/',
+    name: 'Home',
+    component: Home
+    },
+    {
+        path: '/about',
+    name: 'About',
+    component: About
+    }
+  ]
+})
+
+const app = createApp(App)
+app.use(router).mount('#app')
+app.mount('app')
