f058222b8edb78fe9f739a64a9543512b86ca999
you@example.com <you@example.com>
2023-03-16 13:47:09 +0100

    New

---
+++ src/components/AdminPanel.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="Login">
+      Hey
+    </div>
+</template>
+
+
+
+<script>
+import anything from './Login.vue' 
+
+export default {
+    components: {
+        AdminPanel
+    },
+
+    name: 'AdminPanel',
+    props: {
+        username: anything,
+    }
+}
+</script>
